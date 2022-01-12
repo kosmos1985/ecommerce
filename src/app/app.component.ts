ccef0a8822fbfa2b3d06df2ab87fc199a6a0c996
maciej.kosmela <maciej.kosmela@enigma.com.pl>
2022-01-12 07:50:02 +0100

    add angular material, ivy gallery, componets and images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import {Gallery} from 'angular-gallery'
+
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +14,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ecommerce';
+  tiles: Tile[] = [
+    {text: 'One', cols: 3, rows: 1, color: 'lightblue'},
+    {text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
+    {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
+    {text: 'Four', cols: 2, rows: 1, color: '#DDBDF1'},
+  ];
+
+  constructor(private gallery: Gallery) {}
+
+  showGallery(index: number) {
+    let prop = {
+        images: [
+            {path: 'assets/images/image-product-1.jpg'},
+            {path: 'assets/images/image-product-2.jpg'},
+            {path: 'assets/images/image-product-3.jpg'},
+            {path: 'assets/images/image-product-4.jpg'},
+        ],
+        index
+    };
+    this.gallery.load(prop);
+}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './nav/nav/nav.component';
+import { CollectionsComponent } from './content/collections/collections.component';
+import { MenComponent } from './content/men/men.component';
+
+import { WomenComponent } from './content/women/women.component';
+import { AboutComponent } from './content/about/about.component';
+import { ContactComponent } from './content/contact/contact.component';
+import { IvyGalleryModule } from 'angular-gallery';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    CollectionsComponent,
+    MenComponent,
+    WomenComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    IvyGalleryModule
   ],
   providers: [],
   bootstrap: [AppComponent]
