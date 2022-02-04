3fbe40a0fd41b828189baea38ec1b02ca8083934
maciej.kosmela <maciej.kosmela@enigma.com.pl>
2022-02-04 10:35:16 +0100

    add put methods to update contact data

--- src/app/components/content/contact/contact.component.ts
+++ src/app/components/content/contact/contact.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Contact } from 'src/app/models/contact';
+import { CollectionsService } from 'src/app/services/collections.service';
 
 @Component({
   selector: 'app-contact',
@@ -10,7 +12,7 @@ export class ContactComponent implements OnInit {
 
   fields: FormGroup;
 
-  constructor(private fb:FormBuilder) { 
+  constructor(private fb:FormBuilder, private collectionService: CollectionsService) { 
 
     this.fields = this.fb.group({
       name: this.fb.control('', [Validators.required, Validators.minLength(2)]),
@@ -22,11 +24,19 @@ export class ContactComponent implements OnInit {
   }
 
   ngOnInit(): void {
+
   }
 
   onSubmit(){
     if(this.fields.valid){
-
+      const contactData: Contact = {
+        id: 7,
+        name: this.fields.value.name,
+        email: this.fields.value.email,
+        contact_number: this.fields.value.contact_number,
+        message: this.fields.value.message
+      }
+      this.collectionService.putContactData(contactData).subscribe();
     }
     this.fields.reset();
   }
--- src/app/services/collections.service.ts
+++ src/app/services/collections.service.ts
@@ -1,8 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { About } from '../models/about';
 import { Collection } from '../models/collection';
+import { Contact } from '../models/contact';
 import { Map } from '../models/map';
 
 @Injectable({
@@ -18,28 +20,32 @@ export class CollectionsService {
 
   constructor(private http: HttpClient) { }
  
-  getCollections() {
+  getCollections(): Observable<Collection[]>{
     return  this.http.get<Collection>(this.BASE_URL).pipe(map(arr => arr.sort((a: Collection, b: Collection) => a.company === b.company ? 0 : a.company ? 1 : -1)));
   };
 
-  getMenCollection(){
+  getMenCollection(): Observable<Collection[]>{
     return this.http.get<Collection>(this.BASE_URL).pipe(map(items => items.filter(item => item.sex === "Men")));
   };
 
-  getWomenCollection(){
+  getWomenCollection(): Observable<Collection[]>{
     return this.http.get<Collection>(this.BASE_URL).pipe(map(items => items.filter(item => item.sex === 'Women')));
   };
 
-  getItem(id:number) {
+  getItem(id:number): Observable<Collection> {
     return this.http.get<Collection>(this.BASE_URL + id);
   };
   
-  getAbout(){
+  getAbout(): Observable<About>{
     return this.http.get<About>(this.BASE_URL + 5);
   };
 
-  getMap(){
+  getMap(): Observable<Map>{
     return this.http.get<Map>(this.BASE_URL + 6);
   };
+
+  putContactData(contactData: Contact): Observable<Contact>{
+    return this.http.put<Contact>(this.BASE_URL + '/' +  contactData.id, contactData)
+  }
 
 }
